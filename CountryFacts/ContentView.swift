4b90373d6e590a3e6ec2e1cf14236101b5b550c5
Amit Sen <amit.ron.cse@gmail.com>
2025-11-01 10:47:11 +0100

    Initial Commit

---
+++ CountryFacts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CountryFacts
+//
+//  Created by Amit Sen on 01.11.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CountryFacts/CountryFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountryFactsApp.swift
+//  CountryFacts
+//
+//  Created by Amit Sen on 01.11.25.
+//
+
+import SwiftUI
+
+@main
+struct CountryFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
